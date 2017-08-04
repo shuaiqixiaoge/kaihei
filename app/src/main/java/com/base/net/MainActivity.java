2ea3b3c6cdb7bbca58f1265c3a5d0811dd2129a4
yeqiu <1294054822@qq.com>
2017-08-04 10:52:40 +0800

    11

--- app/src/main/java/com/base/net/MainActivity.java
+++ app/src/main/java/com/base/net/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("TAg","testttttttffff");
+        Log.e("TAg","testttttttffffffffff");
     }
 }
