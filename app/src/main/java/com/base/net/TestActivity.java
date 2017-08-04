4f086b85669b894c7af298ec5309d532ece82395
yeqiu <1294054822@qq.com>
2017-08-04 11:27:39 +0800

    新增

---
+++ app/src/main/java/com/base/net/TestActivity.java
@@ -0,0 +1,15 @@
+package com.base.net;
+
+import android.util.Log;
+
+/**
+ * Created by zwy on 2017/8/4.
+ * package_name is com.base.net
+ * 描述:SJ
+ */
+
+public class TestActivity {
+    void main(){
+        Log.e("TAg","小乔是大厦币");
+    }
+}
